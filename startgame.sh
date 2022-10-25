ca7ad882fb5dba1c88f78dd9c2749b08ea60139f
Vince Iannelli <viannelli@gmail.com>
2022-10-25 13:23:52 -0600

    Updated README and starting script

--- startgame.sh
+++ startgame.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/bin/zsh
 
 cd src
 
@@ -6,10 +6,16 @@ FILE='./server-running.txt'
 
 if [ -f "$FILE" ];
 then 
+cd front-end
 node index.js
 else
-node server.js & touch './server-running.txt'
+touch './server-running.txt'
+cd back-end
+node server.js &
+wait
+cd ../front-end
 node index.js
 fi
 
 
+    
